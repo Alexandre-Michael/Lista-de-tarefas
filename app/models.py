b10e755ebe9eae4b211a924019207ff81ec29e3c
Alexandre Michael <alexandremi2024@gmail.com>
2024-12-30 02:07:49 -0300

    🗃️ Added get_user_model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Lista_tarefas(models.Model):
 
@@ -10,6 +11,8 @@ class Lista_tarefas(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     done = models.CharField(max_length=7, choices=STATUS, default='Fazendo')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    #on_delete=models.CASCADE = se o usuário for deletado, as tarefas dele também serão deletadas
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
