91de44b675b1bd973d3f991733e595e50379d640
Alexandre Michael <alexandremi2024@gmail.com>
2025-02-14 15:04:22 -0300

    🧑‍💻 Improvement to ensure loading of .env file

--- lista_tarefas/settings.py
+++ lista_tarefas/settings.py
@@ -13,8 +13,12 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+# path to the .env file
+# ensure .env is loaded
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+
+# Load environment variables
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
